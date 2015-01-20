0d55355322a0062d282205a39493643dfa7625e7
Ryan McLeod <psystorm21@hotmail.com>
2015-01-20 13:28:54 -0500

    Added Ryan()

--- src/helloworld/cpd4414/pkg02/HelloWorldCPD441402.java
+++ src/helloworld/cpd4414/pkg02/HelloWorldCPD441402.java
@@ -29,4 +29,8 @@ public class HelloWorldCPD441402 {
         System.out.println("Amanda Cohoon");
     }
     
+    public static void Ryan() {
+        System.out.println("Hi guys");
+    }
+    
 }
