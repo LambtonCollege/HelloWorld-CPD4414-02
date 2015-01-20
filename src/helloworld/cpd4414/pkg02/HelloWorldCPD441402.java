f82e510281c6ffd6b4050adc069792f8c9e93eef
AustinCoombesReid <austin4@live.ca>
2015-01-20 13:42:43 -0500

    Mah Branch

--- src/helloworld/cpd4414/pkg02/HelloWorldCPD441402.java
+++ src/helloworld/cpd4414/pkg02/HelloWorldCPD441402.java
@@ -29,6 +29,6 @@ public class HelloWorldCPD441402 {
         System.out.println("Amanda Cohoon");
     }
     public static void austin () {
-    System.out.println("WOOO I HAVE A PALACE!!... with blackjack and hookers...Dylans fault");
+    System.out.println("WOOO I HAVE A PALACE!! with doritos and mountain dew..");
 }
 }
