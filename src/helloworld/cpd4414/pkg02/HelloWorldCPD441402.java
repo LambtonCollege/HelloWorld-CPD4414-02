035a76e52fe352d2156c5cd89b72d2ca8579ca63
Len Payne <len.payne@lambtoncollege.ca>
2015-01-20 12:37:17 -0500

    Initial Commit

---
+++ src/helloworld/cpd4414/pkg02/HelloWorldCPD441402.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package helloworld.cpd4414.pkg02;
+
+/**
+ *
+ * @author c0587637
+ */
+public class HelloWorldCPD441402 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Len Payne");
+    }
+    
+}
